981c7a6f7145e048beb9422cdf06759a0498874b
Allan Salandanan <Allan@Moniques-MacBook-Pro.local>
2017-05-09 21:20:26 -0700

    commented out unfinished files

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var passport = require("./config/passport");
 
 // Setting up port and requiring models for syncing
 var PORT = process.env.PORT || 9000;
-var db = require("./models");
+// var db = require("./models");
 
 // Creating express app and configuring middleware needed for authentication
 var app = express();
@@ -16,17 +16,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
 // We need to use sessions to keep track of our user's login status
-app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 // Requiring our routes
-require("./routes/html-routes.js")(app);
-require("./routes/api-routes.js")(app);
+// require("./routes/html-routes.js")(app);
+// require("./routes/api-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
-db.sequelize.sync().then(function() {
+// db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
     console.log("Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
   });
-});
+// });
