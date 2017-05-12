987521832176abf77bfc90216e402620fdc8533a
Allan Salandanan <Allan@Moniques-MacBook-Pro.local>
2017-05-11 18:26:01 -0700

    created index.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var passport = require("./config/passport");
 
 // Setting up port and requiring models for syncing
 var PORT = process.env.PORT || 9000;
-// var db = require("./models");
+var db = require("./models");
 
 // Creating express app and configuring middleware needed for authentication
 var app = express();
@@ -25,8 +25,8 @@ require("./routes/html-routes.js")(app);
 // require("./routes/api-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
-// db.sequelize.sync().then(function() {
+db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
     console.log("Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
   });
-// });
+});
