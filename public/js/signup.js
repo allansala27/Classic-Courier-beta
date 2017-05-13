86d6917dfc56d7c8338814b217759a5e641ada68
telescope_user <telescope_user@Telescopes-MacBook-Pro-4.local>
2017-05-13 13:23:46 -0700

    updated stuff

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,39 +1,45 @@
 $(document).ready(function() {
   // Getting references to our form and input
   var signUpForm = $("form.signup");
-  var accountInput = $("input#account-input");
-  var webidInput = $("input#webid-input");
-  var passwordInput = $("input#password-input");
+  var company = $("input#company-input");
+  var name = $("input#company-input");
+  var address = $("input#address-input");
+  var city = $("input#city-input");
+  var state = $("input#state-input");
+  var zip = $("input#zip-input");
+  var phone = $("input#phone-input");
+  var fax = $("input#fax-input");
+  var email = $("input#email-input");
+  var creditcard = $("input#creditcard-input");
+  var expdate = $("input#expdate-input");
+  var billzip = $("input#billzip-input");
 
   // When the signup button is clicked, we validate the account, webid and password are not blank
   signUpForm.on("submit", function(event) {
     event.preventDefault();
     var userData = {
-      account: accountInput.val().trim(),
-      webid: webidInput.val().trim(), 
-      password: passwordInput.val().trim(),
       company: companyInput.val().trim(),
       name: nameInput.val().trim(),
       address: addressInput.val().trim(),
       city: cityInput.val().trim(),
-      state: stateInput,
+      state: stateInput.val(),
       zip: zipInput.val().trim(),
       phone: phoneInput.val().trim(),
       fax: fax.val().trim(),
       email: email.val().trim(),
+      creditcard: creditcard.val().trim(),
+      expdate: expdate.val().trim(),
+      billzip: billzip.val().trim()
     };
 
-    if (!userData.account || !userData.webid || !userData.password || !userData.company || 
-        !userData.name || !userData.address || !userData.city || !userData.city || !userData.state || 
-        !userData.zip || !userData.phone || !userData.fax || !userData.email) {
+    if (!userData.company || !userData.name || !userData.address || !userData.city || !userData.city || !userData.state || 
+        !userData.zip || !userData.phone || !userData.fax || !userData.email || !userData.creditcard || !userData.expdate ||
+        !userData.billzip) {
       return;
     }
     // If we have an account, webid and password, run the signUpUser function
-    signUpUser(userData.account, userData.webid, userData.password, userData.company, userData.name, userData.address, 
+    signUpUser(userData.company, userData.name, userData.address, 
       userData.city, userData.state, userData.zip, userData.phone, userData.fax, userData.email);
-    accountInput.val("");
-    webidInput.val("");
-    passwordInput.val("");
     companyInput.val("");
     nameInput.val("");
     addressInput.val("");
@@ -43,15 +49,15 @@ $(document).ready(function() {
     phoneInput.val("");
     faxInput.val("");
     emailInput.val("");
+    creditcard.val("");
+    expdate.val("");
+    billzip.val("")
   });
 
   // Does a post to the signup route. If succesful, we are redirected to the members page
   // Otherwise we log any errors
-  function signUpUser(account, webid, password, company, name, address, city, state, zip, phone, fax, email) {
+  function signUpUser(company, name, address, city, state, zip, phone, fax, email, creditcard, expdate, billzip) {
     $.post("/api/signup", {
-      account: account,
-      webid: webid,
-      password: password,
       company: company,
       name: name,
       address: address,
@@ -60,7 +66,10 @@ $(document).ready(function() {
       zip: zip,
       phone: phone,
       fax: fax,
-      email: email
+      email: email,
+      creditcard: creditcard,
+      expdate: expdate,
+      billzip: billzip
     };
      //not sure if this is the correct redirect
     }).then(function(data) {
