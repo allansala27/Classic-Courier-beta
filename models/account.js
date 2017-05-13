7eb97f2bcc45c24a185083825efc4bb2a262b959
Wyatt Thomson <wyattthomson@Wyatts-MacBook-Pro.local>
2017-05-13 13:02:14 -0700

    established database

--- models/account.js
+++ models/account.js
@@ -1,6 +1,16 @@
 module.exports = function(sequelize, DataTypes) {
   var Account = sequelize.define("Account", {
 
+  	Company: DataTypes.STRING,
+  	Name: DataTypes.STRING,
+  	Company: DataTypes.STRING,
+  	Name: DataTypes.STRING,
+  	City: DataTypes.STRING,
+  	State: DataTypes.STRING,
+  	Zip_code: DataTypes.STRING,
+  	Telephone: DataTypes.STRING,
+  	Fax: DataTypes.STRING,
+  
   	//enter account columns
 
   })
--- server.js
+++ server.js
@@ -3,16 +3,15 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var session = require("express-session");
 var path = require('path');
+var Sequelize = require('sequelize');
 
 // Requiring passport as we've configured it
 // var passport = require("./config/passport");
 
-// Setting up port and requiring models for syncing
-var PORT = process.env.PORT || 9000;
-// var db = require("./models");
-
 // Creating express app and configuring middleware needed for authentication
 var app = express();
+var PORT = process.env.PORT || 9000;
+var db = require("./models");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -27,10 +26,10 @@ require('./routes')(app);
 
 
 // Syncing our database and logging a message to the user upon success
-// db.sequelize.sync().then(function() {
+db.sequelize.sync().then(function() {
 app.listen(PORT, function() {
     console.log("Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
 });
-// });
+});
 
 module.exports = app;
