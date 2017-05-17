0765ad49710514154d26fd44aa31e4d6c6d3b54f
Pickles <pafitschen@gmail.com>
2017-05-16 19:54:14 -0700

    order info

--- models/login.js
+++ models/login.js
@@ -1,16 +1,11 @@
 module.exports = function(sequelize, DataTypes) {
   var LoginInfo = sequelize.define("LoginInfo", {
 
-<<<<<<< HEAD
-  	account: DataTypes.INTEGER,
-  	webid: DataTypes.STRING,
-  	password: DataTypes.STRING,
-  	//enter account columns
-=======
+
   	account: DataTypes.STRING,
   	webid: DataTypes.STRING,
   	password: DataTypes.STRING,
->>>>>>> master
+
 
   })
   return LoginInfo;
