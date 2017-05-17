ce1189f9072317a12df8b3e9967f4518d6670acc
Allan Salandanan <Allan@Moniques-MacBook-Pro.local>
2017-05-16 20:51:15 -0700

    fixed routing

--- routes.js
+++ routes.js
@@ -5,9 +5,13 @@ module.exports = function(app){
         // Our model controllers (rather than routes)
         var guest = require('./controllers/guest');
         var member = require('./controllers/member');
+        var signup = require('./controllers/signup_controller');
+        var order = require('./controllers/order_controller');
 
         app.use('/', guest);
         app.use('/member', member);
+        app.use('/signup', signup);
+        app.use('/member/order', order);
 
         // Set Handlebars as the default templating engine.
 		app.engine("handlebars", exphbs({ defaultLayout: "main" }));
