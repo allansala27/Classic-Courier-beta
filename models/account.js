12acf5dd8527027eb7aa973ec8a54bafd4584f89
Allan Salandanan <Allan@Moniques-MacBook-Pro.local>
2017-05-18 18:53:14 -0700

    signup database link

--- models/account.js
+++ models/account.js
@@ -1,17 +1,18 @@
 module.exports = function(sequelize, DataTypes) {
-  var Account = sequelize.define("Account", {
+  var Accounts = sequelize.define("Accounts", {
 
   	Company: DataTypes.STRING,
   	Name: DataTypes.STRING,
     Address: DataTypes.STRING,
   	City: DataTypes.STRING,
   	State: DataTypes.STRING,
-  	Zip_code: DataTypes.STRING,
+  	Zip: DataTypes.STRING,
   	Telephone: DataTypes.STRING,
   	Fax: DataTypes.STRING,
+    Email:DataTypes.STRING
   
   	//enter account columns
 
   })
-  return Account;
+  return Accounts;
 };  
--- public/js/signup.js
+++ public/js/signup.js
@@ -1,69 +1,46 @@
 $(document).ready(function() {
-  // Getting references to our form and input
-  var company = $("input#company-input");
-  var name = $("input#name-input");
-  var address = $("input#address-input");
-  var city = $("input#city-input");
-  var state = $("input#state-input");
-  var zip = $("input#zip-input");
-  var telephone = $("input#telephone-input");
-  var fax = $("input#fax-input");
-  var email = $("input#email-input");
+    $(".btn-primary").on("click", function(event) {
+            // event.preventDefault();
+            //store info in vars
+            var userCompany = $("#company-input").val();
+            var userName = $("#name-input").val();
+            var userAddress = $("#address-input").val();
+            var userCity = $("#city-input").val();
+            var userState = $("#state-input").val();
+            var userZip = $("#zip-input").val();
+            var userTelephone = $("#telephone-input").val();
+            var userFax = $("#fax-input").val();
+            var userEmail = $("#email-input").val();
+        
+        console.log(userCompany, userName, userAddress, userCity, userState, userZip, userTelephone, userFax, userEmail);
+        // checks for empty fields
+        if (!userCompany || !userName || !userAddress || !userCity || !userState || !userZip || !userTelephone || !userFax) {
+            return;
+        }
 
-  // When the signup button is clicked, we validate the account, webid and password are not blank
-  $(".submit").on("click", function(event) {
-    event.preventDefault();
-    var userData = {
-      company: company.val().trim(),
-      name: name.val().trim(),
-      address: address.val().trim(),
-      city: city.val().trim(),
-      state: state.val(),
-      zip: zip.val().trim(),
-      telephone: telephone.val().trim(),
-      fax: fax.val().trim(),
-      email: email.val().trim(),
-    };
-
-    if (!userData.company || !userData.name || !userData.address || !userData.city || !userData.city || !userData.state || 
-        !userData.zip || !userData.telephone || !userData.fax || !userData.email || !userData.creditcard || !userData.expdate ||
-        !userData.billzip) {
-      return;
-    }
-    // If we have an account, webid and password, run the signUpUser function
-    signUpUser(userData.company, userData.name, userData.address, 
-      userData.city, userData.state, userData.zip, userData.telephone, userData.fax, userData.email);
-    company.val("");
-    name.val("");
-    address.val("");
-    city.val("");
-    state.val("");
-    zip.val("");
-    telephone.val("");
-    fax.val("");
-    email.val("");
-  });
-
-  // Does a post to the signup route. If succesful, we are redirected to the members page
-  // Otherwise we log any errors
-  function signUpUser(company, name, address, city, state, zip, telephone, fax, email, creditcard, expdate, billzip) {
-    $.post("/signup", {
-      company: company,
-      name: name,
-      address: address,
-      city: city,
-      state: state,
-      zip: zip,
-      telephone: telephone,
-      fax: fax,
-      email: email
-    };
-     //not sure if this is the correct redirect
-    }).then(function(data) {
-      window.location.replace(data);
-    }).catch(function(err) {
-      console.log(err);
+        // If all fields are filled in run signUpUser function
+        signUpUser(userCompany, userName, userAddress, userCity, userState, userZip, userTelephone, userFax, userEmail);
     });
-  }
+
+// Does a post to the signup route. If succesful, we are redirected to the members page
+// Otherwise we log any errors
+function signUpUser(company, name, address, city, state, zip, telephone, fax, email) {
+    $.post("/signup", {
+        company: company,
+        name: name,
+        address: address,
+        city: city,
+        state: state,
+        zip: zip,
+        telephone: telephone,
+        fax: fax,
+        email: email
+            //not sure if this is the correct redirect
+    }).then(function(data) {
+        window.location = data.redirect;
+    }).catch(function(err) {
+        console.log(err);
+    });
+}
 
 });
