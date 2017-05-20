9c0dd858f7b056f8074f1fc9f216c85da57072c1
Allan Salandanan <allansala27@gmail.com>
2017-05-20 11:57:39 -0700

    fixing3

--- controllers/login_controller.js
+++ controllers/login_controller.js
@@ -14,7 +14,7 @@ router.post('/', function(req, res) {
         webId: req.body.webId
     }).then(function(res) {
         console.log(res);
-    res.redirect('/member/order');
+    res.send({ redirect: '/member/order' });
 })
         //compare form entry to decrypted
         //if all good, res.redirect
